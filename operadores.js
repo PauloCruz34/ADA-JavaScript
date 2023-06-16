6dc9474e58aba60dcc5d5398e50a55948469ca9b
Paulo Afonso da Cruz <126684518+PauloCruz34@users.noreply.github.com>
2023-06-16 12:07:12 -0300

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,25 @@
+/* Operadores Aritméticos */
+
+
+let num_1 = 30; /* posso escrever a variável sempre com letra minúscula no  começo */
+let num_2 = 60
+
+console.log("Soma = ", num_1 + num_2);   /* + - / ** % */
+console.log("subtração", num_1 - num_2)
+console.log("multiplicação", num_1 * num_2)
+console.log("Divisão", num_1 / num_2)
+console.log("Exponenciação", num_1 ** num_2)
+console.log("Resto da divisão", num_1 % num_2)
+
+
+/* Precedencia é a ordem das operçãoes, primeiro o parenteses mais interno e depois e assim sucessivamente */
+console.log((30+25)*(3*(25-6)))
+
+console.log(Math.PI)    /* Math traz funções matemáticas */
+console.log(Math.sqrt(16))  /* volta a raiz quadrada */
+
+/* pesquisar java script no MDM la volta todas as propriedades de cada função dentro da biblioteca */
+
+
+
+
